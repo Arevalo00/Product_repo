15be305d98c878c39a8e527394edd90a6b6feacb
jennifer arevalo <arevalo5007@gmail.com>
2023-08-17 13:59:02 -0500

    added the category routes and fixed tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,108 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  try {
+    const categoryData = await Product.findAll({
+      include: [
+        Product, 
+        {
+          module: Category,
+       
+        }
+      ],
+    });
+  
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  try {
+    const categoryData = await Product.findOne({
+      where: {id: req.params.id },
+      include: [
+        Product, 
+        {
+          module: Category,
+        
+        }
+      ],
+    });
+  if (!categoryData) {
+    res.status(400).json({message: 'No Product Found With That ID'})
+  }
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  });
 
-router.post('/', (req, res) => {
+
+
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categoryData = await Category.create({
+      category_name: req.body.category_name  
+    });
+    res.status(200).json(categoryData);
+  } catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryDate = await Category.update(req.body,{
+      where:{
+        id: req.params.id
+      },
+
+    });
+    if (!categoryDate){
+      res.status(404).json({message: 'No ID'});
+      return;
+    }
+    res.status(200).json(categoryDate);
+    
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+    
+  }
+
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.findOneAndDelete({
+      where: {id: req.params.id },
+      
+    });
+  if (!categoryData) {
+    res.status(400).json({message: 'No Product Found With That ID'})
+  }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+
+
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
 try { 
   const tagData = await Tag.findAll({
-    include:  {model: Product, through: productTag},
+    include:  {model: Product, through: ProductTag},
   });
   res.status(200).json(tagData); 
   
@@ -21,10 +21,15 @@ try {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  try {
+  try { 
+    const tagData = await Tag.findOne({
+      include:  {model: Product, through: productTag},
+    });
+    res.status(200).json(tagData); 
     
   } catch (err) {
-    
+    console.log(err);
+   res.status(500).json(err); 
   }
 });
 
@@ -47,11 +52,11 @@ router.put('/:id', async (req, res) => {
   try {
     const tagDate = await Tag.update(req.body,{
       where:{
-        tag_id: req.params.tag_id
+        id: req.params.id
       },
 
     });
-    if (!tagData[0]){
+    if (!tagDate){
       res.status(404).json({message: 'No ID'});
       return;
     }
@@ -67,14 +72,14 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await tag.destroy({
+    const tagData = await Tag.findOneAndDelete({
       where:{
-        tag_id: req.params.tag_id,
+        id: req.params.id,
       }
       
     });
   if (!tagData){
-    res.status(404).json({message: 'No ID'});
+    res.status(400).json({message: 'No ID'});
     return;
   }
    res.status(200).json(tagData); 
